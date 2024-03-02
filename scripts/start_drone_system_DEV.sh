37f0184bb234279939a8a366095cde391ab482b4
Kailash Khadka <k.khadka343@gmail.com>
2024-03-01 22:11:39 -0500

    Moved from lopho_project to Lopho

--- scripts/start_drone_system_DEV.sh
+++ scripts/start_drone_system_DEV.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Change directory to the project directory
-cd /home/$(whoami)/lopho_project
+cd /home/$(whoami)/Lopho
 
 # Get today's date in YYYYMMDD format
 date_tag=$(date +'%Y%m%d')
@@ -9,11 +9,11 @@ date_tag=$(date +'%Y%m%d')
 # Build the Docker image with the tag including the date
 docker rm $(docker container ls -aq) -f
 docker rm humble_container -f
-docker build -t humble_machine:$date_tag /home/$(whoami)/lopho_project/.devcontainer/
+docker build -t humble_machine:$date_tag /home/$(whoami)/Lopho/.devcontainer/
 
 # Display a message indicating the start of Docker run
 echo "Docker Run"
-docker run -itd --privileged --name humble_container --user vscode --network host --ipc host -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -v $PWD:/lopho_project humble_machine:20240301
+docker run -itd --privileged --name humble_container --user vscode --network host --ipc host -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -v $PWD:/Lopho humble_machine:20240301
 
 # Start SITL in a new terminal
 gnome-terminal -- bash -c \
@@ -27,7 +27,7 @@ gnome-terminal -- bash -c \
 gnome-terminal -- bash -c \
   "docker exec -it humble_container bash -c ' \
     echo \"Terminal MAVROS for \$(whoami)\"; \
-    source /lopho_project/ros2_ws/install/setup.bash ; \
+    source /Lopho/ros2_ws/install/setup.bash ; \
     source /opt/ros/${ROS_DISTRO}/setup.bash; \
     ros2 launch mavros apm.launch fcu_url:=tcp://localhost gcs_url:=udp://@localhost:14550; \
     bash';"
