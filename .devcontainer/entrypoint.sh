3b029ab63da86fa14efdedb17f64bbdc821c8bb8
Kailash Khadka <k.khadka343@gmail.com>
2024-04-22 03:40:50 +0000

    Restructured directory

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,5 +1,38 @@
 #!/bin/bash
 set -e  # Exit immediately if a command exits with a non-zero status
-echo "Executing entrypoint.sh"
+mkdir -p /home/$(whoami)/Lopho/ros2_ws/src
+cd /home/$(whoami)/Lopho/ros2_ws
+# source underlay
+source /opt/ros/${ROS_DISTRO}/setup.bash
+
+# build creates folder structure with ../ros_ws/src folder among others.
+colcon build --symlink-install 
+
+# check for dependencies
+# rosdep install -i --from-path src --rosdistro ${ROS_DISTRO} -y 
+
+cd src
+
+# to this folder clone package source files
+
+# go back to root folder & build package
+cd /home/$(whoami)/Lopho/ros2_ws
+colcon build --packages-select lopho_pkg
+source install/local_setup.bash
+ros2 run lopho_pkg my_node
+
+# TODO: Add git clone for different packages and node here
+
+
+
+# source /home/${USERNAME}/Lopho/ros2_ws/install/setup.bash
+# echo "source /home/${USERNAME}/Lopho/ros2_ws/install/setup.bash" >> ~/.bashrc
+# echo "source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash" >> ~/.bashrc
+# echo "source /usr/share/colcon_cd/function/colcon_cd.sh" >> ~/.bashrc
+# echo "export _colcon_cd_root=/opt/ros/humble/" >> ~/.bashrc
+
+
+# echo $(which source)
+# touch "/home/$(whoami)/Lopho/masala.sdf"
 
 exec "$@"
--- scripts/start_drone_system_DEV.sh
+++ scripts/start_drone_system_DEV.sh
@@ -15,27 +15,27 @@ docker build -t humble_machine:$date_tag /home/$(whoami)/Lopho/.devcontainer/
 echo "Docker Run"
 docker run -itd --privileged --name humble_container --user vscode --network host --ipc host -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -v $PWD:/Lopho humble_machine:$date_tag
 
-# Start SITL in a new terminal
-gnome-terminal -- bash -c \
-  "docker exec -it humble_container bash -c ' \
-    echo \"Terminal SITL for \$(whoami)\"; \
-    echo \"Starting SITL\"; \
-    cd simulation; \
-    arducopter -S -I0 --home 43.502922,-80.467015,0,353 --model '+' --speedup 1 --defaults /home/\$(whoami)/ardupilot/Tools/autotest/default_params/copter.parm; \
-    bash';"
+# # Start SITL in a new terminal
+# gnome-terminal -- bash -c \
+#   "docker exec -it humble_container bash -c ' \
+#     echo \"Terminal SITL for \$(whoami)\"; \
+#     echo \"Starting SITL\"; \
+#     cd simulation; \
+#     arducopter -S -I0 --home 43.502922,-80.467015,0,353 --model '+' --speedup 1 --defaults /home/\$(whoami)/ardupilot/Tools/autotest/default_params/copter.parm; \
+#     bash';"
 
-gnome-terminal -- bash -c \
-  "docker exec -it humble_container bash -c ' \
-    echo \"Terminal MAVROS for \$(whoami)\"; \
-    source /Lopho/ros2_ws/install/setup.bash ; \
-    source /opt/ros/${ROS_DISTRO}/setup.bash; \
-    ros2 launch mavros apm.launch fcu_url:=tcp://localhost gcs_url:=udp://@localhost:14550; \
-    bash';"
+# gnome-terminal -- bash -c \
+#   "docker exec -it humble_container bash -c ' \
+#     echo \"Terminal MAVROS for \$(whoami)\"; \
+#     source /Lopho/ros2_ws/install/setup.bash ; \
+#     source /opt/ros/${ROS_DISTRO}/setup.bash; \
+#     ros2 launch mavros apm.launch fcu_url:=tcp://localhost gcs_url:=udp://@localhost:14550; \
+#     bash';"
 
-gnome-terminal -- bash -c \
-  "docker exec -it humble_container bash -c ' \
-    echo \"Terminal QGC for \$(whoami)\"; \
-    QGC';" 
+# gnome-terminal -- bash -c \
+#   "docker exec -it humble_container bash -c ' \
+#     echo \"Terminal QGC for \$(whoami)\"; \
+#     QGC';" 
 
 gnome-terminal -- bash -c \
   "docker exec -it humble_container bash" 
